24e4053a8c2b8f036682725b6a5110ffa5e0a653
Tafsirchy <tafsirchy1000@gmail.com>
2026-08-23 17:55:45 +0600

    Add JWT settings model

---
+++ backend/Models/JwtSettings.cs
@@ -0,0 +1,10 @@
+namespace EShopManager.API.Models
+{
+    public class JwtSettings
+    {
+        public string Secret { get; set; } = null!;
+        public string Issuer { get; set; } = null!;
+        public string Audience { get; set; } = null!;
+        public int ExpirationInMinutes { get; set; } = 60;
+    }
+}
